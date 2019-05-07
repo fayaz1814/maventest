7355b1e7012b460471478f5ffdb4da58513d2968
fayaz1814 <fayaz.mirza.shaik@gmail.com>
2019-05-07 13:39:58 +0000

    quickstartjar/

---
+++ quickstartjar/src/main/java/fayazadda/App.java
@@ -0,0 +1,13 @@
+package fayazadda;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World! and how are you?"); 
+    }
+}
